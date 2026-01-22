aacb55774d695b9032754bb067507275a34d2280
Santiago <santiagodeandrade4@gmail.com>
2026-01-22 15:07:50 -0400

    Creacion de script boceto

--- main.py
+++ main.py
@@ -1,50 +1,38 @@
-
 import torch
 from pathlib import Path
-from anomalib.data import MVTec
-from anomalib.models import EfficientAD
+from anomalib.data import MVTecAD
+from anomalib.models import EfficientAd
 from anomalib.engine import Engine
 
-
-def main():
+if __name__ == "__main__":
 
     print("Version de pytorch: ", torch.__version__)
-    print("Grafica disponible?: {'Si' if torch.cuda.is_available() else 'No'}", )
+    print(f"Grafica disponible?: {'Si' if torch.cuda.is_available() else 'No'}", )
     print("Version de cuda: ", torch.version.cuda)
     if torch.cuda.is_available(): 
         print("Nombre de grafica: ", torch.cuda.get_device_name(0))
+        if '5070' in torch.cuda.get_device_name(0):
+            torch.set_float32_matmul_precision('high')
 
 
-    data_root = Path(r"./data/mvtec_anomaly_detection/")
+    data_root = Path(r"./data/")
     category = "bottle"
-    category_img_res = (900,900)
-
-    # 1) DataModule: MVTec (ya calza con tu estructura train/test/ground_truth)
-    datamodule = MVTec(
+    datamodule = MVTecAD(
         root=data_root,
         category=category,
-        image_size=category_img_res,       # común para empezar; luego lo ajustas
-        train_batch_size=16,
-        eval_batch_size=16,
-        num_workers=8,
-        task="segmentation",         # porque tienes ground_truth masks
+        train_batch_size=1,  # recomendado para empezar con EfficientAd
+        eval_batch_size=1,
+        num_workers=8
     )
 
-    model = EfficientAD(
-        image_size=category_img_res,
-    )
+    model = EfficientAd()
 
     engine = Engine(
-        task="segmentation",
-        accelerator="auto",  # usa GPU si está disponible
+        accelerator="auto",
         devices=1,
     )
 
-    # Entrenar
     engine.fit(datamodule=datamodule, model=model)
-
-    # Evaluar en test (usa test/* y ground_truth/* para métricas pixel-level)
     engine.test(datamodule=datamodule, model=model)
 
-if __name__ == "__main__":
-    main()
+
